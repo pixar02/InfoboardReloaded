04d5ece80947a0ea05fc602f9243e95b6f9ae1b3
BuildTools <unconfigured@null.spigotmc.org>
2017-07-18 01:47:49 +0200

    Worlguard and FileManager

---
+++ InfoboardReload/src/com/pixar02/infoboard/APIS/WorlGuard.java
@@ -0,0 +1,34 @@
+package com.pixar02.infoboard.APIS;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class WorlGuard {
+	public static boolean boardsAreAllowedHere(Location loc) {
+		boolean allowed = true;
+		if (serverHasWorldGuard()) {
+
+			return allowed;
+		}
+		return false;
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
+	 * Does the server have WorldGuard? returns true/false
+	 * 
+	 */
+	private static boolean serverHasWorldGuard() {
+		if (Bukkit.getPluginManager().getPlugin("Worlguard") != null) {
+			return true;
+		}
+		return false;
+	}
+}
