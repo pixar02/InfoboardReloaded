83af81d637bb13875bce9e2b212b480631bc1a71
pixar02 <pixar.pf@gmail.com>
2017-09-09 18:56:21 +0200

    debug in playerjoin removed

--- InfoboardReload/src/com/pixar02/infoboard/Events/PlayerJoin.java
+++ InfoboardReload/src/com/pixar02/infoboard/Events/PlayerJoin.java
@@ -24,39 +24,4 @@ public class PlayerJoin implements Listener {
 		}
 	}
 
-	@EventHandler
-	public void onPlayerJoinEvent(PlayerJoinEvent event) {
-		Player player = event.getPlayer();
-		player.sendMessage("Its Working!");
-
-		Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(plugin, new Runnable() {
-			int i;
-
-			public void run() {
-				// for (i = 0; i <= 6; i++) {
-				if (i <= 6) {
-					if (i == 0) {
-						player.sendMessage("Its Working! %number%".replaceAll("%number%", Integer.toString(i)));
-					} else if (i == 1) {
-						player.sendMessage("Its Working! %number%".replaceAll("%number%", Integer.toString(i)));
-					} else if (i == 2) {
-						player.sendMessage("Its Working! %number%".replaceAll("%number%", Integer.toString(i)));
-					} else if (i == 3) {
-						player.sendMessage("Its Working! %number%".replaceAll("%number%", Integer.toString(i)));
-					} else if (i == 4) {
-						player.sendMessage("Its Working! %number%".replaceAll("%number%", Integer.toString(i)));
-					} else if (i == 5) {
-						player.sendMessage("Its Working! %number%".replaceAll("%number%", Integer.toString(i)));
-					} else if (i == 6) {
-						player.sendMessage("Its Working! %number%".replaceAll("%number%", Integer.toString(i)));
-					}
-				} else {
-					Bukkit.getScheduler().cancelTasks(plugin);
-				}
-
-			}
-		}, 0, 100);
-
-	}
-
 }
